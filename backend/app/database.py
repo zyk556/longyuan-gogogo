70b9721644af6cde7b995c5d0c64e3cb697fe19d
zyk556 <1042436785@qq.com>
2026-06-12 13:18:26 +0800

    2

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False, pool_size=5, max_overflow=10)
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
